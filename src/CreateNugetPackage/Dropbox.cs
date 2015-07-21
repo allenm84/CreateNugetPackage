6a0f5601e25b1bebd7b59e2d6ffdd8ab9b61bdbc
Michael Allen <michael.allen@cognex.com>
2015-07-21 16:08:02 -0500

    Updated to actually create the nuget package and publish it to Dropbox\[nuget]

---
+++ src/CreateNugetPackage/Dropbox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace System.Common.References
+{
+  public static class Dropbox
+  {
+    private static Lazy<string> location = new Lazy<string>(() =>
+    {
+      var folders = new[]
+      {
+        Environment.SpecialFolder.ApplicationData,
+        Environment.SpecialFolder.CommonApplicationData,
+        Environment.SpecialFolder.LocalApplicationData,
+      };
+
+      foreach (var folder in folders)
+      {
+        string appData = Environment.GetFolderPath(folder);
+        string hostdb = Path.Combine(appData, @"Dropbox\host.db");
+        if (!File.Exists(hostdb)) continue;
+        string[] lines = File.ReadAllLines(hostdb);
+        return Encoding.ASCII.GetString(Convert.FromBase64String(lines[1]));
+      }
+
+      throw new FileNotFoundException("host.db not found");
+    }, true);
+
+    public static string Location { get { return location.Value; } }
+
+    static Dropbox() { }
+  }
+}
---
+++ src/CreateNugetPackage/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Common.References;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateNugetPackage
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      string path = Application.StartupPath;
+      string dropbox = Dropbox.Location;
+
+      var nuget = Path.Combine(dropbox, @"Apps\nuget\nuget.exe");
+
+      var repositories = Directory.EnumerateDirectories(path);
+      foreach (var repo in repositories)
+      {
+        var bin = Path.Combine(repo, "bin");
+        if (!Directory.Exists(bin))
+        {
+          Console.WriteLine("{0} does not contain a bin directory", repo);
+          continue;
+        }
+
+        var spec = Directory.EnumerateFiles(repo, "*.nuspec", SearchOption.TopDirectoryOnly).SingleOrDefault();
+        if (string.IsNullOrWhiteSpace(spec))
+        {
+          Console.WriteLine("{0} does not contain a nuspec", repo);
+          continue;
+        }
+
+        var dir = new DirectoryInfo(repo);
+        var output = Path.Combine(dropbox, "[nuget]", dir.Name);
+        var info = new ProcessStartInfo(nuget, string.Format("pack -OutputDirectory \"{0}\"", output));
+        info.WorkingDirectory = repo;
+        Process.Start(info).WaitForExit();
+      }
+    }
+  }
+}
